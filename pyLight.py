edd61049a5a98f3ebcb1fbcf928a9e4aadc35605
Alex Eshoo <AlexEshoo@gmail.com>
2017-08-28 22:23:53 -0400

    Made Some changes and it kind of does something.

---
+++ pyLight.py
@@ -0,0 +1,18 @@
+import serial
+import time
+import struct
+
+comm = serial.Serial('COM4', 115200, timeout=.1)
+inp = [0] * 90
+
+while True:
+    msg = b''
+    if inp[0] == 0:
+        inp = [10] * 90
+    else:
+        inp = [0] * 90
+
+    for n in range(len(inp)):
+        msg += struct.pack('B', inp[n])
+    comm.write(msg)
+    time.sleep(1)
