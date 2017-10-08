3d3557884943c94f0c9e99c633189224e79d47c0
Alex Eshoo <AlexEshoo@gmail.com>
2017-10-08 16:05:15 -0400

    Added basic volume level display functionality as demo

--- pyLight.py
+++ pyLight.py
@@ -6,10 +6,27 @@ class KeyboardController(object):
     def __init__(self, com):
         self.strip = Strip(com)
         keyboard.hook(self.event_hook)
+        self.volume_level = 0
 
     def event_hook(self, event):
-        event.name
-        if len(keyboard._pressed_events) > 0:
+        print(event.name)
+        if event.name == "volume up" and event.event_type == "up":
+            self.volume_level += 1
+            for k in range(self.strip.LEN):
+                if self.volume_level > k:
+                    self.strip._send_color(31,31,31)
+                else:
+                    self.strip._send_color(0,0,0)
+        elif event.name == "volume down" and event.event_type == "down":
+            self.volume_level -= 1
+            for k in range(self.strip.LEN):
+                if self.volume_level > k:
+                    self.strip._send_color(31,31,31)
+                else:
+                    self.strip._send_color(0,0,0)
+        elif 'volume' in event.name:
+            pass
+        elif len(keyboard._pressed_events) > 0:
             self.strip.send_uniform_color(10,0,0)
         else:
             self.strip.send_uniform_color()
