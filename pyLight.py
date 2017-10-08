a86c96a0538fd3feddf5c343499737bbfdf99770
Alex Eshoo <AlexEshoo@gmail.com>
2017-10-07 21:59:24 -0400

    Merge branch 'serializeFunction' into dev
    restructed

--- pyLight.py
+++ pyLight.py
@@ -2,20 +2,23 @@ import serial
 import time
 import struct
 
-# Arduino Board Resets everytime the serial communication is activated and deactivated.
-# This means that when this script starts there will be a delay before the arduino starts
-# responding. Keeping DTR High or Low seems to have no effect.
-
-comm = serial.Serial('COM4', 115200, timeout=.1)
-time.sleep(2) # WAIT FOR RESET TO OCCUR ON SERIAL CONNECT
+def connectArduino(COM):
+    # Arduino Board Resets everytime the serial communication is activated and deactivated.
+    # This means that when this script starts there will be a delay before the arduino starts
+    #responding. Keeping DTR High or Low seems to have no effect.
+    comm = serial.Serial(COM, 115200, timeout=0.1)
+    time.sleep(2) # Allow Ardunio reset to happen to prevent buffer offset
+    
+    return comm
 
 def serializeColor(R,G,B,X=False):
     """
     Packs color values into two serial bytes to be read by the arduino.
     
-    :param R: Red Value from 0 to 31
-    :param G: Green Value from 0 to 31
-    :param B: Blue Value from 0 to 31
+    :param R: Red Value int from 0 to 31
+    :param G: Green Value int from 0 to 31
+    :param B: Blue Value int from 0 to 31
+    :param X: Extra bit. Unused for now. (bool)
     :returns: Packed bytes object of length 2
     """
     try:
@@ -37,15 +40,21 @@ def serializeColor(R,G,B,X=False):
     
     return num
 
-i=0
-while True:
-    c = serializeColor(i,i,i)
-    off = serializeColor(0,0,0)
-    for k in range(30):
-        if k % 2 == 0:
-            comm.write(c)
-        else:
-            comm.write(off)
-    if i < 30: i += 1
-    else: i = 0
-    time.sleep(0.008) # Fastest you can go without overflow ~= 0.008 (125Hz)
+def sendColor(com, serColor):
+    com.write(serColor)
+    
+def test():
+    i=0
+    while True:
+        c = serializeColor(i,i,i)
+        OFF = serializeColor(0,0,0)
+        for k in range(30):
+            if k % 2 == 0:
+                sendColor(comm, c)
+            else:
+                sendColor(comm, OFF)
+        if i < 30: i += 1
+        else: i = 0
+        time.sleep(0.008) # Fastest you can go without overflow ~= 0.008 (125Hz)
+
+comm = connectArduino('COM4')
