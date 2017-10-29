dcb066ae477e73d5af1d10c9fc1edc45d9388595
Alex Eshoo <AlexEshoo@gmail.com>
2017-10-29 17:11:06 -0400

    Added better performing idle functions

--- strip_driver/strip_driver.ino
+++ strip_driver/strip_driver.ino
@@ -10,11 +10,9 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, PIN, NEO_GRB + NEO_KHZ800);
 void setup() {
   strip.begin();
   strip.show();
+  strip.setBrightness(255); // Future use for setting max brightness
 
   Serial.begin(115200);
-  while (Serial.available() > 0) {
-    Serial.read();
-  }
 }
 
 void loop() {
@@ -28,7 +26,7 @@ void loop() {
                    // on new incoming data buffer.
 
   if (millis() - timer > 5000) {
-    breath(10);
+    rainbowCycle(5);
   }
   
   while (Serial.available() > 59) {
@@ -83,15 +81,53 @@ void breath(int dt) {
   }
 }
 
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256 * 5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+      if (Serial.available() > 0) {
+        for (uint16_t k=0; k<strip.numPixels(); k++) {
+          strip.setPixelColor(k, strip.Color(0,0,0));
+        }
+        return;
+      }
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+      if (Serial.available() > 0) {
+        for (uint16_t k=0; k<strip.numPixels(); k++) {
+          strip.setPixelColor(k, strip.Color(0,0,0));
+        }
+        return;
+      }
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
 uint32_t Wheel(byte WheelPos) {
   WheelPos = 255 - WheelPos;
   if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3,0);
   }
   if(WheelPos < 170) {
     WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3,0);
   }
   WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0,0);
 }
