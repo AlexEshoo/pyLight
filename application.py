38d99a88d52dbc2d015c3a38d70dbd170c72c2ec
Alex Eshoo <AlexEshoo@gmail.com>
2018-03-07 22:30:23 -0500

    changed to using threading and events. GUI apply works now.

--- application.py
+++ application.py
@@ -3,6 +3,8 @@ from gui import Ui_MainWindow
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtCore import QThread
+import threading
+import time
 
 class PyLightApp(Ui_MainWindow):
     def __init__(self, window):
@@ -21,17 +23,48 @@ class PyLightApp(Ui_MainWindow):
             self.controlModeComboBox.addItem(key)
 
     def doApply(self):
+        print("Applying")
         control = self.controlModeComboBox.currentText()
         if self.controller:
+            print("Controller exists...")
             self.worker.stop()
+            self.worker.join()
 
         self.controller = pyLight.CONTROL_MODES[control]
 
-        self.worker = workerThread(self.controller)
+        self.worker = WorkerThread(self.controller)
         self.worker.start()
+        print("end")
 
 
-class workerThread(QThread):
+class WorkerThread(threading.Thread):
+    """"
+    Only works with Screenshot controller at the moment. Need to implement
+    better class structure to approach this from a polymorphic angle.
+    """
+    def __init__(self, worker):
+        super().__init__()
+        self.worker = worker
+        self._stop_event = threading.Event()
+
+    def run(self):
+        self.controller = self.worker("COM4")
+
+        while not self._stop_event.is_set():
+            self.controller.send_major_color()
+
+        self.controller.disconnect()
+
+    def stop(self):
+        self._stop_event.set()
+
+
+class QTworkerThread(QThread):
+    """
+    This does not work... I may do more investigating later or just
+    continue to use built in threading.
+    Perhaps threading.event can be used with Qthread objects?
+    """
     def __init__(self, worker):
         QThread.__init__(self)
         self.worker = worker
@@ -41,6 +74,18 @@ class workerThread(QThread):
 
     def run(self):
         self.controller = self.worker('COM4')
+        #print("starting")
+        #while True:
+            #time.sleep(1)
+            #print("THis is happening!")
+
+    def stop(self):
+        print("Stopping")
+        #self.controller.controlling = False
+        print("disconnecting")
+        #self.controller.disconnect()
+        print("terminating")
+        self.terminate()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
